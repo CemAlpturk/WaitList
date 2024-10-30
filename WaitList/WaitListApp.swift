bc1b2c20c9e32a43281191e5ee30c5c40b3e6562
Cem Alpturk <cem.alpturk@gmail.com>
2024-10-30 19:12:01 +0100

    Initial Commit

---
+++ WaitList/WaitListApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaitListApp.swift
+//  WaitList
+//
+//  Created by Cem Alptürk on 2024-10-30.
+//
+
+import SwiftUI
+
+@main
+struct WaitListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
